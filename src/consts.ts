54dab38304d392d51b5582c68f70c2a0fe161727
Lucas ROURET <lucas.rouretperso@gmail.com>
2025-09-09 22:22:43 +0200

    update words

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@ import type { IconMap, Site, SocialLink } from '@/types'
 export const SITE: Site = {
   title: 'Lucas Rouret',
   description:
-    "Software engineer passionate developer and entrepreneur based in France. I build modrn web & mobile applications. I love 'clean' code, great user experiences, and solving real-world problems through tech.",
+    'Passionate about software development, I adapt quickly to any environment and learn fast. I prioritize methodology and continuous improvement over overproduction, convinced that every mistake or delay is a learning opportunity. Curious and versatile, a true Swiss Army knife, I am seeking an exciting professional experience where I can tackle diverse challenges and deliver real value.',
   href: 'https://www.lucas-rouret.fr',
   author: 'Lucas Rouret',
   locale: 'en-US',
